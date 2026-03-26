a5ee4f451d0fe2ff7d7ba70f17d3792b34fc2d23
Dharshan8806 <dk8806@srmist.edu.in>
2026-03-26 10:00:30 +0530

    Create OOPSBannerApp.java

---
+++ OOPSBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * Printing OOPS
+ * @author Dharshan
+ * @version 1.1
+ */
+
+public class OOPSBannerApp{
+    public static void main(String[] args) {
+        System.out.println(String.join(" ", "*****", " ","*****", " ", "*****", " ", "*****"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*   *", " ", "*    "));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*   *", " ", "*    "));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*****", " ", "*****"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*    ", " ", "    *"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*    ", " ", "    *"));
+        System.out.println(String.join(" ", "*****", " ", "*****", " ", "*    ", " ", "*****"));                
+    }
+}
